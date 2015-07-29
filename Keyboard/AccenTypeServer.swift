ea98d999c1dd7ecc6223b49c011be9f057c54e8f
Markus Cozowicz <eisber@eisber.net>
2015-07-29 12:02:17 -0400

    added documentation

--- Keyboard/AccenTypeServer.swift
+++ Keyboard/AccenTypeServer.swift
@@ -10,6 +10,9 @@ import Foundation
 import CocoaAsyncSocket
 import PriorityQueue
 
+/**
+    Communication backend to retrieve suggestions.
+*/
 @objc
 class AccenTypeServer: GCDAsyncUdpSocketDelegate {
     var server: String;
@@ -21,6 +24,12 @@ class AccenTypeServer: GCDAsyncUdpSocketDelegate {
     var socket:GCDAsyncUdpSocket!;
     var requestId: UInt16;
 
+    /**
+        Initializes AccenTypeServer.
+    
+        :param: server  The server used to retrieve suggestions.
+        :param: port    The port used to communicate with server.
+    */
     init (server: String, port: CUnsignedShort) {
         self.server = server
         self.port = port
@@ -31,6 +40,9 @@ class AccenTypeServer: GCDAsyncUdpSocketDelegate {
         setupConnection()
     }
     
+    /**
+        Setup UDP connection.
+    */
     func setupConnection() {
         var error : NSError?
         self.socket = GCDAsyncUdpSocket(delegate: self, delegateQueue: dispatch_get_main_queue())
@@ -43,7 +55,10 @@ class AccenTypeServer: GCDAsyncUdpSocketDelegate {
         self.init(server: "accentypeheader.cloudapp.net", port: 10100)
     }
     
-    func clearTimedOut() {
+    /**
+        Clears any timed out request.
+    */
+    func clearTimedOutRequests() {
         let now = NSDate()
         
         var oldest = self.requestQueue.peek()
@@ -54,6 +69,15 @@ class AccenTypeServer: GCDAsyncUdpSocketDelegate {
         }
     }
     
+    /**
+        Convenience function to read from buffer.
+    
+        :param: data    The buffer to read from.
+        :param: target  The variable to write to.
+        :param: offset  The offset to read from. Gets updated on successful read.
+        :param: length  Number of bytes to read. Defaults to sizeof(UInt8).
+        :returns:       True if read was successful, false otherwise.
+    */
     func read(data:NSData, target:UnsafeMutablePointer<Void>, inout offset:Int, length:Int = sizeof(UInt8)) -> Bool {
         if (offset + length > data.length) {
             return false;
@@ -65,8 +89,15 @@ class AccenTypeServer: GCDAsyncUdpSocketDelegate {
         return true
     }
     
+    /**
+        Callback triggered by GCDAsyncUdpSocket. Guaranteed to run on GCD.
+    */
     func udpSocket(udpSocket: GCDAsyncUdpSocket!, didReceiveData data: NSData!, fromAddress: NSData!, withFilterContext: AnyObject!) {
-        self.clearTimedOut()
+        self.clearTimedOutRequests()
+      
+        if (udpSocket == nil || data == nil) {
+            return;
+        }
         
         var offset: Int = 0;
        
@@ -125,7 +156,7 @@ class AccenTypeServer: GCDAsyncUdpSocketDelegate {
     func getSuggestion(text: String, completion: (result: [[String]]) -> Void) {
         // make sure there is no concurrent access
         dispatch_async(dispatch_get_main_queue()) {
-            self.clearTimedOut()
+            self.clearTimedOutRequests()
             
             var localRequestId = self.requestId++
 
@@ -142,6 +173,9 @@ class AccenTypeServer: GCDAsyncUdpSocketDelegate {
         }
     }
 
+    /**
+        Internal class to track outstanding requests.
+    */
     class Request {
         var id: UInt16;
         var completion: (result: [[String]]) -> Void;
